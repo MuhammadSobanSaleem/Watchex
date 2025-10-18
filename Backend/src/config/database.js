876b24e1beaa9c228e9954155bf95ec40c188816
Muhammad Soban Saleem <sobansaleem713@gmail.com>
2025-10-18 16:13:14 +0500

    adjust for deployment

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function connectDB(){
     try{
 
-        const connection = await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PWD}@cluster0.yufdedk.mongodb.net/${process.env.DB_NAME}`)
+        const connection = await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log(`Database Connected Successfully`)
 
     }catch(err){
