80911e7b27a2fde650d68ebd9ffb0c3e699431ff
Muhammad Soban Saleem <sobansaleem713@gmail.com>
2025-10-29 23:01:57 +0500

    modified MongoDB URI

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function connectDB(){
     try{
 
-        const connection = await mongoose.connect(`${process.env.MONGO_URI}`)
+        const connection = await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yufdedk.mongodb.net/${process.env.MONGO_DB}`)
         console.log(`Database Connected Successfully`)
 
     }catch(err){
